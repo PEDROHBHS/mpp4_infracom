9f07035e2c3dea6a561dcf0b5652f91773e58242
Pedro Henrique <boahorapedro@gmail.com>
2022-10-30 22:20:17 -0300

    Audio with video

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -4,7 +4,7 @@ from moviepy.editor import *
 import multiprocessing
 from playsound import playsound
 import os
-
+import threading
 
 class VideoPlayer:
     def __init__(self, parent : Text, video_file : str):
@@ -23,13 +23,15 @@ class VideoPlayer:
         frame.pack_propagate(False)
         frame.pack()
         video_mp3 = VideoFileClip(self.video)
-        video_mp3.audio.write_audiofile(f'test.mp3')
-                
+        base, ext = os.path.splitext(self.video)
+        video_mp3.audio.write_audiofile(f'{base}.mp3')
+
+        p = multiprocessing.Process(target=playsound, args=(f'{base}.mp3',))
+        threading.Thread(target=p.start).start()        
+
         video = TkinterVideo(frame, scaled=True)
         video.load(r"{}".format(self.video))
-        p = multiprocessing.Process(target=playsound, args=(f'test.mp3',))
-        p.start()
-
+        
         def stop_video():
             video.pause()
             video.seek(0)
